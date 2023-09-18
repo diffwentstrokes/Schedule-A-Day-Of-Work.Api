ef3d096fb3cf16406927f501a737c9750d380dd2
diffwentstrokes <rooksdcr@gmail.com>
2023-09-18 11:23:55 -0600

    Added neccesary changes to html file, for updated time blocks Implemented Javascript Fixed errors in syntax like indentation and spelling

--- script.js/script.js
+++ script.js/script.js
@@ -1,53 +1,63 @@
 $(function () {
-  $(document).ready(function()
-{
+
+
+
+loadEvents();
+
+
+$(".saveBtn").on("click", function() {
+   
+    let timeBlockId = $(this).parent().attr("id");
+    
+    let hour = timeBlockId.split("-")[1];
+    
+    let description = $(this).siblings(".description").val();
+
+    localStorage.setItem("event_" + hour, description);
+
+});
+
+
+function loadEvents()  {
+    
+    $(".time-block").each(function() {
+
+        let hour = $(this).attr("id").split("-")[1];
+
+        let eventText = localStorage.getItem("event_"  + hour);
+        
+        $(this).find(".description").val(eventText);
+    });
+}
+
+function blockColorChange() {
+    
+        let currentHour = parseInt(dayjs().format("H"));
+
+    
+    $(".time-block").each(function() {
+        
+        let userHour = parseInt($(this).attr("id").split("-")[1]);
+        
+        if (currentHour === userHour) {
+        $(this).addClass("present");
+      } else if (currentHour < userHour) {
+        $(this).addClass("future");
+      } else {
+        $(this).addClass("past");
+      }
+    });
+  }
+
   
-  $('.saveBtn').on('click', function(){
-      let clickedButton = $(this);
-
-      let timeblock = clickedButton.closest('.time-block');
-
-      let hour = timeblock.attr('id');
-
-      let userDescription = timeblock.find('textarea.description').val();
-
-      localStorage.setItem(hour, userDescription);
+  $(document).ready(function() {
+    blockColorChange();
   });
 
-    const currentHour = dayjs().hour();
-
-    $('.time-block').each(function() {
-    let block = $(this);  
-    
-    let blockHour = parseInt(block.attr('id').split('-')[1]);
-
-     if (blockHour < currentHour) {
-        block.addClass('past').removeClass('present future');
-    } else if (blockHour === currentHour) {
-        block.addClass('present').removeClass('past future');
-    } else {
-        block.addClass('future').removeClass('past present');
-    }
-});
-
-$('.time-block').each(function() {
-    
-    let blockID = $(this).attr('id');
-    
-    
-    let savedText = localStorage.getItem(blockID);
-    
-    
-    if (savedText) {
-        $(this).find('textarea').val(savedText);
-    }
-});
-
-$("#currentDay").text(dayjs().format('MMMM D, YYYY'));
-
-});
-});
-
   
-
-
+  const currentDate = dayjs();
+  
+  
+  const formattedDate = currentDate.format('dddd, MMMM D');
+  document.querySelector('#currentDay').textContent = formattedDate;
+}); 
